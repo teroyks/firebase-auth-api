d0f231c01722a28ae44fb348860b7948176ae7f0
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2019-03-27 11:25:33 +0200

    Add firebase authentication
    - accepts any authenticated user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,58 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
 import express from 'express'
 
+admin.initializeApp()
 const app = express()
 
-app.get(`/api`, (req, res) => {
+// enable adding the user token to request
+declare global {
+  namespace Express {
+    interface Request {
+      user?: admin.auth.DecodedIdToken
+    }
+  }
+}
+
+const denyAccess = (res: express.Response) => {
+  res.status(403).send(`Unauthorized`)
+}
+
+// Validate Firebase ID Tokens
+const authenticate = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith(`Bearer `)
+  ) {
+    denyAccess(res)
+    return
+  }
+
+  const idToken = req.headers.authorization.split(`Bearer `)[1]
+  console.log(`id token: ${idToken}`)
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedIdToken => {
+      req.user = decodedIdToken
+      console.log(`authenticated user:`, decodedIdToken)
+      next()
+    })
+    .catch(() => {
+      console.log(`Got invalid token`)
+      denyAccess(res)
+      return
+    })
+}
+
+app.use(authenticate)
+
+app.get(`/api`, (req: express.Request, res: express.Response) => {
   res.send(`${Date.now()}`)
 })
 
